1f2f20b43ccff57a3ebc12b81d9911a0bca8969a
leon.chen <happylennychen@outlook.com>
2019-01-28 08:40:11 +0800

    Removed transportation part

---
+++ System/Common/O2Micro.Cobra.Common/CobraGlobal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace O2Micro.Cobra.Common
+{
+    public static class CobraGlobal
+    {
+        public static class Constant
+        {
+            public const string OldBoardConfigName = "BoardConfig";
+            public const string NewBoardConfigName = "Board Config";
+        }
+    }
+}
